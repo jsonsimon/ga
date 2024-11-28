0e9527928463a265cb12826804cc29308b247c6e
Simon Jansson <simon@json.nu>
2024-11-28 23:12:53 +0100

    clean: removed old debug prints

--- test.py
+++ test.py
@@ -19,16 +19,10 @@ for row in rows:
 		d[datastat] = txt
 	stats.append(d)
 	count = count+1
-#print(stats)
 
 for statdict in stats:
 	if (statdict["score"] != ""):
 		print(statdict["home_team"], statdict["home_xg"], statdict["score"], statdict["away_xg"], statdict["away_team"])
 
-#table=table.replace("Match Report", "\n")
-#table=table.replace("Head-to-Head", "\n")
-
-#print(table)
-#print(row[1])
 
 
