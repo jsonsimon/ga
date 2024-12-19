285d155600e8e9df41449a51e581dcd2778a40c5
Vincent Lundmark <lundmark.vincent@gmail.com>
2024-12-19 11:50:57 +0100

    feat: sumdf now contains winner column

--- matplotlibtest.py
+++ matplotlibtest.py
@@ -2,17 +2,43 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 plt.style.use('bmh')
-df = pd.read_csv('sumdftest.csv')
+df = sumdf = pd.concat(map(pd.read_csv, ["table2021-2022.csv", "table2022-2023.csv", "table2023-2024.csv", "table2024-2025.csv"]), ignore_index=True)
 
+winner = []
+sumdf = sumdf[sumdf["score"].notna()] #removal of games not yet played
+manuloc = sumdf["score"].str.replace("–", "-") #html "-"" to math "-" to use eval
+gd  = manuloc.apply(eval) 
+sumdf["attendance"] = sumdf["attendance"].str.replace(",", "") #removal of "," to make strings into ints
+sumdf["attendance"] = sumdf["attendance"].fillna(0) #switching of "NaN" to "0" to make strings into ints
+sumdf["date"] = pd.to_datetime(sumdf["date"]) #making date inte pandas date
+
+sumdf["gd"] = gd #add goal difference to dataframe
+
+for index, row in sumdf.iterrows():
+    if row['gd'] < 0: 
+        winner.append(row['away_team'])
+    elif row['gd'] < 0: 
+        winner.append(row['home_team'])
+    else:
+        winner.append('tie')
+win = pd.DataFrame(winner)
+
+sumdf["winner"] = win
+
+
+
+
+'''
 # All teams
-x = df['team']
-y = df['winrate']
+x = df['date']
+y = df['winner']
 
 # Bar chart
-plt.xlabel('team', fontsize=4)
-plt.ylabel('winrate(%)', fontsize=16)
+plt.xlabel('date', fontsize=18)
+plt.ylabel('winner', fontsize=16)
 plt.bar(x, y)
 
 plt.show()
 
 # Den blev as skev men testa bara lite
+'''
